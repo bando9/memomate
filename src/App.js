3d638a28f49b89be3bd74ba8169fb1bc60bcaabc
bando9 <bandomega1@gmail.com>
2024-12-01 11:10:43 +0700

    feat: add NoteCard component for displaying note content
    - Includes title, text, date, and action icons (delete & archive)
    
    - Basic styling and responsive layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header.js";
+import NoteCard from "./components/NoteCard.js";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <NoteCard />
     </div>
   );
 }
